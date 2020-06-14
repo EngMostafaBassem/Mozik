d3c8d58576a8c889aea732e86e2792dacc70f7af
Mostafa <mostafa_basem@hotmail.com>
2020-06-14 18:13:35 +0200

    Updates

---
+++ precache-manifest.43131e0d1d6ae5a8e81b37f938e5f166.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffcb00e6a60d1975171f6a6709b7c75c",
+    "url": "/Mozik/index.html"
+  },
+  {
+    "revision": "7d7add8610bf71167ed0",
+    "url": "/Mozik/static/css/2.301ce841.chunk.css"
+  },
+  {
+    "revision": "74f90ca1adf401f26bc2",
+    "url": "/Mozik/static/css/main.6b88c009.chunk.css"
+  },
+  {
+    "revision": "7d7add8610bf71167ed0",
+    "url": "/Mozik/static/js/2.4ff8b428.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Mozik/static/js/2.4ff8b428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f90ca1adf401f26bc2",
+    "url": "/Mozik/static/js/main.0c9d818c.chunk.js"
+  },
+  {
+    "revision": "be68a9776f6f3a11f51e",
+    "url": "/Mozik/static/js/runtime-main.b71472d1.js"
+  },
+  {
+    "revision": "52a51c9c4461bd654be32b328963c9ac",
+    "url": "/Mozik/static/media/01_girls-doing-stuff-playing-drums.52a51c9c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Mozik/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Mozik/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Mozik/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Mozik/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Mozik/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
